c136cb6180e1bf72dd30014d66ffd41333a9160b
Khanh Nguyen <khanhnguyen02311@gmail.com>
2025-08-10 00:51:46 +0700

    fix: apply lint, move interactive apps to separate installation option

--- scripts/pre-setup/fzf/fzf.sh
+++ scripts/pre-setup/fzf/fzf.sh
@@ -6,6 +6,6 @@ set -ex
 
 # get current script path
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-PKGMGR="$(${SCRIPT_DIR}/../../get-os-info.sh --pkg)"
+PKGMGR="$("${SCRIPT_DIR}/../../get-os-info.sh" --pkg)"
 
 sudo $PKGMGR fzf
