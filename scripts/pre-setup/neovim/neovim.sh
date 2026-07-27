ca0400c2cb195f02bb21a0a44c53a3bce2cb10a3
Khanh Nguyen <khanhnguyen02311@gmail.com>
2026-07-28 00:21:46 +0700

    feat: add alacritty config, update setup.sh entrypoint

--- scripts/pre-setup/neovim/neovim.sh
+++ scripts/pre-setup/neovim/neovim.sh
@@ -1,14 +1,32 @@
 #!/usr/bin/env bash
 
-set -ex
+set -x
 
-[[ -n "$(command -v nvim)" ]] && { echo "NeoVim already installed. Exiting. "; exit 0; }
+cleanup_nvim() {
+    sudo rm -rf "$HOME/.local/bin/nvim"
+    rm -rf "$HOME/.local/share/nvim"
+    rm -rf "$HOME/.local/state/nvim"
+}
 
-# get current script path
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-curl -Lo "${SCRIPT_DIR}/nvim-linux-x86_64.tar.gz" "https://github.com/neovim/neovim/releases/download/v0.11.7/nvim-linux-x86_64.tar.gz"
-# curl -Lo "${SCRIPT_DIR}/nvim-linux-x86_64.tar.gz" "https://github.com/neovim/neovim/releases/download/stable/nvim-linux-x86_64.tar.gz"
-tar xf "${SCRIPT_DIR}/nvim-linux-x86_64.tar.gz" -C "${SCRIPT_DIR}"
+setup_nvim() {
+    # get current script path
+    SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+    curl -Lo "${SCRIPT_DIR}/nvim-linux-x86_64.tar.gz" "https://github.com/neovim/neovim/releases/download/v0.11.7/nvim-linux-x86_64.tar.gz"
+    # curl -Lo "${SCRIPT_DIR}/nvim-linux-x86_64.tar.gz" "https://github.com/neovim/neovim/releases/download/stable/nvim-linux-x86_64.tar.gz"
+    tar xf "${SCRIPT_DIR}/nvim-linux-x86_64.tar.gz" -C "${SCRIPT_DIR}"
 
-## create symlink
-sudo ln -sf "${SCRIPT_DIR}/nvim-linux-x86_64/bin/nvim" "$HOME/.local/bin/nvim"
+    ## create symlink
+    sudo ln -sf "${SCRIPT_DIR}/nvim-linux-x86_64/bin/nvim" "$HOME/.local/bin/nvim"
+}
+
+if [[ -z "$(command -v nvim)" ]]; then  # new setup
+    setup_nvim
+else
+    read -p "NeoVim already installed. Reinstall? (y/N): " reinstall
+    if [[ "${reinstall,,}" == "y" ]]; then
+        cleanup_nvim
+        setup_nvim
+    else
+        echo "Keeping old version. Exiting."
+    fi
+fi
--- setup.sh
+++ setup.sh
@@ -4,13 +4,24 @@ set -e
 
 INSTALL_APPS=
 
-print_help () {
+printlog() {
+  case "$1" in
+    "info")     { printf "\033[0;34m${2}\033[0m\n"; } ;;
+    "info2")    { printf "\033[0;36m${2}\033[0m\n"; } ;;
+    "warning")  { printf "\033[0;33m${2}\033[0m\n"; } ;;
+    "danger")   { printf "\033[0;31m${2}\033[0m\n"; } ;;
+    "success")  { printf "\033[0;32m${2}\033[0m\n"; } ;;
+    *)          { echo "Invalid echo color"; return 1; } ;;
+  esac
+}
+
+printhelp () {
   echo "setup.sh: Setup dotfiles script."
   echo ""
   echo "Syntax: ./setup.sh [-a][-h]"
   echo "Arguments:"
-  echo "  -a | --with-apps: install system apps, mainly for interactive systems like personal machine. You can skip it for setup on servers / non-interactive-login systems."
-  echo "  -h | --help: show help. "
+  echo "  -a | --with-apps: install system apps, mainly for interactive systems like personal machine. You can skip it for setup on servers / non-interactive systems."
+  echo "  -h | --help     : show help. "
 }
 
 while (( "$#" )); do
@@ -20,12 +31,12 @@ while (( "$#" )); do
         shift
         ;;
       -h|--help)
-        print_help
+        printhelp
         exit 0
         ;;
       -*)
         echo "Error: Unknown options '$1'"
-        print_help
+        printhelp
         exit 1
         ;;
       *)
@@ -46,36 +57,44 @@ done
 # }
 #
 
+printlog info "===== SETUP.SH ====="
+
 # # Create ~/.local/bin/ and export to PATH if it doesn't exist
 if [ ! -d "$HOME/.local/bin" ]; then
   mkdir -p "$HOME/.local/bin"
   echo 'export PATH="$HOME/.local/bin:$PATH"' >> "$HOME/.bashrc"
 fi
 
-echo -e "\n==== Pre-dotfile-setup process ====\n"
+printlog info "\n==== Pre-dotfile-setup process ====\n"
+
 PRE_SETUP_SCRIPTS=$(find scripts/pre-setup -maxdepth 2 -type f -name "*.sh")
 while IFS= read -r line; do
+  printlog info2 "Running ${line}"
   eval "$line"
 done <<< "${PRE_SETUP_SCRIPTS}"
 
 if [[ -n "${INSTALL_APPS}" ]]; then
-  echo -e "\n==== '-a' option is enabled, installing specified apps ====\n"
+  printlog info2 "\n==== '-a' option is enabled, installing specified apps ====\n"
   APP_SCRIPTS=$(find scripts/interactive-apps -maxdepth 2 -type f -name "*.sh")
   while IFS= read -r line; do
+    printlog info2 "Running ${line}"
     eval "$line"
   done <<< "${APP_SCRIPTS}"
 fi
 
-echo -e "\n==== Loading dotfiles to their respective locations ====\n"
+printlog info "\n==== Loading dotfile resources ====\n"
+
 # remove existed configs that are not Stow's symlink
 git checkout main
-[[ -n $(git status --ignore-submodules=dirty | grep "Changes not staged for commit") ]] && ( echo "You should stage/commit edited files before running the scripts, as the script is going to restore the git HEAD. Exiting."; exit 1; )
+[[ -n $(git status --ignore-submodules=dirty | grep "Changes not staged for commit") ]] && ( printlog warning "You should stage/commit edited files before running the scripts, as the script is going to restore the git HEAD. Exiting."; exit 1; )
 stow --adopt --stow .
 git checkout HEAD
 stow --restow .
 
-echo -e "\n==== Post-dotfile-setup process ====\n"
+printlog info "\n==== Post-dotfile-setup process ====\n"
+
 POST_SETUP_SCRIPTS=$(find scripts/post-setup -maxdepth 2 -type f -name "*.sh")
 while IFS= read -r line; do
+  printlog info2 "Running ${line}"
   eval "$line"
 done <<< "${POST_SETUP_SCRIPTS}"
